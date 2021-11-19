92af6bec9522311331bb5993243e58f4fc9b4e0d
PHINX <82601259+PHINXED@users.noreply.github.com>
2021-11-19 19:22:20 +0100

    Update post.php

--- post.php
+++ post.php
@@ -1,16 +1,13 @@
 <?php
 $db = new mysqli("localhost","root","","passs");
 
-$sql = "SELECT * FROM `post_luis`";
+$sql = "SELECT * FROM posts_luis";
 
 $result = $db->query($sql);
 
 
 while ($row = $result->fetch_assoc()){
-    if (@$usuario == @$row['post'] and @$password == @$row['id'])
-    {
-        @$encontrado = true;
-    }
+         echo $row['post'].'<br>';
 }
 
 $result->close();
